c53ce056c76aadfd7d8016d66592ded6c0048802
vijaykumar8564 <104026680+vijaykumar8564@users.noreply.github.com>
2023-08-12 12:09:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-
   skip_provider_registration = "true"
+
+  # Connection to Azure
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
 }
 
 variable "prefix" {
